5b0e9ccd25e918b2c839515e6d48417a8749df84
Dustin <ddemers1995@hotmail.com>
2023-01-07 19:00:27 -0800

    app dupdate

--- teck-app/components/vehicle-cards/vehiclecard1.tsx
+++ teck-app/components/vehicle-cards/vehiclecard1.tsx
@@ -1,7 +1,7 @@
 
 
 const Vehiclecard1 = () => {
-    return ( <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    return ( <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         Vehicle ID: #0074</h5>
     <ul className="font-normal text-gray-700 dark:text-gray-400">
