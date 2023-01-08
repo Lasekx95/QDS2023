7bc6d4338c916eff698b2459cbaf5dbbf6bb0997
Dustin <ddemers1995@hotmail.com>
2023-01-07 19:11:12 -0800

    finished updating darkmode shiiiiieeeet

--- teck-app/components/vehicle-cards/vehiclecard2.tsx
+++ teck-app/components/vehicle-cards/vehiclecard2.tsx
@@ -2,7 +2,7 @@
 
 const Vehiclecard2 = () => {
     return ( <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100">
-    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
         Vehicle ID: #0107</h5>
     <ul className="font-normal text-gray-700 dark:text-gray-400">
     <li>
