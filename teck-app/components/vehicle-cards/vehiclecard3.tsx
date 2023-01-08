f2700911ff5e768dffdfe34fa65e1facf0797b49
Dustin <ddemers1995@hotmail.com>
2023-01-07 19:17:51 -0800

    finished changing dark mode

--- teck-app/components/vehicle-cards/vehiclecard3.tsx
+++ teck-app/components/vehicle-cards/vehiclecard3.tsx
@@ -4,7 +4,7 @@ const Vehiclecard3 = () => {
     return ( <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
         Vehicle ID: #0107</h5>
-    <ul className="font-normal text-gray-700 dark:text-gray-400">
+    <ul className="font-normal text-gray-700">
     <li>
     Driver: Tom Jones
     </li> 
