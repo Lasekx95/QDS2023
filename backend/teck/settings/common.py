6f37c8741098bb436da5780ffac7bde44a7f3fdf
zach <zachflentgewong@gmail.com>
2023-01-08 09:20:50 -0800

    added cors headers

--- backend/teck/settings/common.py
+++ backend/teck/settings/common.py
@@ -33,11 +33,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'corsheaders'
     'rest_framework',
     'playground'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleWare',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -50,6 +52,11 @@ MIDDLEWARE = [
 
 INTERNAL_IPS = [
     '127.0.0.1'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'http://127.0.0.1:8001'
 ]
 
 ROOT_URLCONF = 'teck.urls'
