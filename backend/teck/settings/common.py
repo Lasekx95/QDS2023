95936c5350fc5bfc24ff9019f61a6b63566a76c7
zach <102389340+zw0ng@users.noreply.github.com>
2023-01-09 00:21:19 -0800

    more typos

--- backend/teck/settings/common.py
+++ backend/teck/settings/common.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'corsheaders'
+    'corsheaders',
     'rest_framework',
     'playground'
 ]
