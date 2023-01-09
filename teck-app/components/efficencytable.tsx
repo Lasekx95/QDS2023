2de06c2c10e570732edeb6e31e035e8db0a14aa9
zach <102389340+zw0ng@users.noreply.github.com>
2023-01-08 23:48:48 -0800

    typo in allowed origins

--- teck-app/components/efficencytable.tsx
+++ teck-app/components/efficencytable.tsx
@@ -1,5 +1,26 @@
+import React from 'react';
+import axios from "axios";
+
+interface TruckData {
+  // define the shape of the truck data here
+  truck_id: string;
+  truck_type_id: string;
+}
+
 
 const Efficencytable = () => {
+    const [data, setData] = React.useState<TruckData[]>([]);
+
+  React.useEffect(() => {
+    // make a request to the API to fetch the data
+    axios.get('https://teck-prod.herokuapp.com/playground/data/?format=api').then(response => {
+      setData(response.data);
+    }).catch(error => {
+      console.log(error);
+      // you can also set an error state here
+    });
+  }, []);
+  
     return ( <div className="relative overflow-x-auto">
     <table className="w-full text-sm text-left text-gray-500">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50">
@@ -31,37 +52,33 @@ const Efficencytable = () => {
             </tr>
         </thead>
         <tbody>
-            <tr className="bg-white border-b">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    ID# 01
-                </th>
-                <td className="px-6 py-4">
-                    0
-                </td>
-                <td className="px-6 py-4">
-                    0
-                </td>
-                <td className="px-6 py-4">
-                    0
-                </td>
-                <td className="px-6 py-4">
-                    0
-                </td>
-                <td className="px-6 py-4">
-                    0
-                </td>
-                <td className="px-6 py-4">
-                    0
-                </td>
-                <td className="px-6 py-4">
-                    0
-                </td>
-            </tr>            <tr className="bg-white border-b">
+        
+        {data &&
+  data.map((truck: TruckData, index: number) => (
+    <tr className="bg-white border-b">
+      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+        ID# {truck.truck_id}
+      </th>
+      <td className="px-6 py-4">{truck.truck_type_id}</td>
+      {/* render the rest of the table cells here */}
+    </tr>
+  ))}
+
+        </tbody>
+    </table>
+</div>
+ );
+}
+ 
+export default Efficencytable;
+
+
+                      {/* <tr className="bg-white border-b">
                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                     ID# 02
                 </th>
                 <td className="px-6 py-4">
-                    0
+                    FUCK
                 </td>
                 <td className="px-6 py-4">
                     0
@@ -210,11 +227,4 @@ const Efficencytable = () => {
                 <td className="px-6 py-4">
                     0
                 </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
- );
-}
- 
-export default Efficencytable;
+            </tr> */}
