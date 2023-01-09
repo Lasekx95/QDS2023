67f97ce3327fc9bcac5597964c814c7a926bf56e
zach <102389340+zw0ng@users.noreply.github.com>
2023-01-08 23:53:51 -0800

    added allowed origins

--- backend/teck/settings/common.py
+++ backend/teck/settings/common.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-
+    
 
 # Application definition
 
@@ -57,7 +57,9 @@ INTERNAL_IPS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
     'http://127.0.0.1:8001',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://localhost:3001'
 ]
 
 ROOT_URLCONF = 'teck.urls'
